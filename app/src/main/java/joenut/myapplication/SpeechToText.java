0e7623d6b8b0580e63370afb2ab06925a3457875
pongsakon <pongsakonpromma@gmail.com>
2018-11-23 22:39:03 +0700

    แก้ไปเรื่อยๆ

--- app/src/main/java/joenut/myapplication/SpeechToText.java
+++ app/src/main/java/joenut/myapplication/SpeechToText.java
@@ -37,7 +37,7 @@ public class SpeechToText extends AppCompatActivity {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "ใส่เสียงได้เลยครับ");
         try {
             startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
         } catch (ActivityNotFoundException a) {
